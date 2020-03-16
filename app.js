b87e68112574b73c79161273be9ac0cdcb249b76
edenbergel <eden.bergel@hetic.net>
2020-03-16 13:05:21 +0100

    post formulaire vers l'api

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const app = express();
 
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
 });
 
 app.post('/party', function(req, res) {
-  res.send('Post ok !')
+  axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({data}) => console.log(data))
+    .catch((err) => console.error(err));
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
