3be12a6f2d8f320bbb9070a0f3f28a6cedb5f2f1
edenbergel <eden.bergel@hetic.net>
2020-03-16 12:56:17 +0100

    test post form event

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
 });
 
+app.post('/party', function(req, res) {
+  res.send('Post ok !')
+});
+
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
