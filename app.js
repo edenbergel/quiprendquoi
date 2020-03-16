445a3262e7efb1db714c7e354a542536744df311
edenbergel <eden.bergel@hetic.net>
2020-03-16 12:20:54 +0100

    quatrième partie serveur avec nodemon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('qui prend quoi ?');
+  res.send('qui prend quoi ? eden');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
