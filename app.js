5cf035946bf567e2325c9e7792258f6df1e3f1d9
edenbergel <eden.bergel@hetic.net>
2020-03-16 12:26:12 +0100

    sixième partie add pug view

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dotenv = require('dotenv').config();
 
 app.get('/', function(req, res) {
-  res.send('qui prend quoi ? eden');
+  res.render('index');
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.set('view engine', 'pug');
+
+app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
