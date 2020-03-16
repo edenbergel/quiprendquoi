25e8ca503dd90b6a941617c0243d0c81ca486de8
edenbergel <eden.bergel@hetic.net>
2020-03-16 14:45:18 +0100

    Récupérer l'événément auprès de l'API

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ app.post('/party', function(req, res) {
 });
 
 app.get('/party/:id', function(req, res) {
-  res.render('party', { title: 'Party'} )
+  axios
+  .get(`${process.env.API_URL}/party/${req.params.id}`)
+  .then(({ data }) =>
+    res.render('party', {
+      party: data,
+      title: data.name,
+    }),
+  )
+  .catch((err) => console.log(err));
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
