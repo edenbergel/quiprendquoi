405d6226e9756458533414fb8356cbb58a55f1cd
edenbergel <eden.bergel@hetic.net>
2020-03-22 01:30:52 +0100

    36: Liste des pages disponibles

--- sw.js
+++ sw.js
@@ -10,26 +10,26 @@ addEventListener('fetch', (event) => {
   if (event.request.headers.get('Accept').includes('text/html')) {
     event.respondWith(
       fetch(event.request)
-      .then((res) => {
-        if (isPartyPage(event.request.url)) {
-          const copy = res.clone();
-          caches
-            .open('parties')
-            .then((cache) => cache.put(event.request, copy));
-          return res;
-        } else {
-          return res;
-        }
-      })
-      .catch(() => {
-        if (isPartyPage(event.request.url)) {
-          return caches
-            .match(event.request)
-            .catch((err) => caches.match('offline.html'));
-        } else {
-          return caches.match('offline.html');
-        }
-      })
+        .then((res) => {
+          if (isPartyPage(event.request.url)) {
+            const copy = res.clone();
+            caches
+              .open('parties')
+              .then((cache) => cache.put(event.request, copy));
+            return res;
+          } else {
+            return res;
+          }
+        })
+        .catch(() => {
+          if (isPartyPage(event.request.url)) {
+            return caches
+              .match(event.request)
+              .catch(() => caches.match('offline.html'));
+          } else {
+            return caches.match('offline.html');
+          }
+        }),
     );
   }
 });
