35e92b6810a211d3269cc0119c5b715078cb0a1b
edenbergel <eden.bergel@hetic.net>
2020-03-22 01:36:57 +0100

    37: Mise en cache des autres ressources

--- sw.js
+++ sw.js
@@ -10,26 +10,36 @@ addEventListener('fetch', (event) => {
   if (event.request.headers.get('Accept').includes('text/html')) {
     event.respondWith(
       fetch(event.request)
+      .then((res) => {
+        if (isPartyPage(event.request.url)) {
+          const copy = res.clone();
+          caches
+            .open('parties')
+            .then((cache) => cache.put(event.request, copy));
+          return res;
+        } else {
+          return res;
+        }
+      })
+      .catch(() => {
+        if (isPartyPage(event.request.url)) {
+          return caches
+            .match(event.request)
+            .catch(() => caches.match('offline.html'));
+        } else {
+          return caches.match('offline.html');
+        }
+      }),
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request)
         .then((res) => {
-          if (isPartyPage(event.request.url)) {
-            const copy = res.clone();
-            caches
-              .open('parties')
-              .then((cache) => cache.put(event.request, copy));
-            return res;
-          } else {
-            return res;
-          }
+          const copy = res.clone();
+          caches.open('static').then((cache) => cache.put(event.request, copy));
+          return res;
         })
-        .catch(() => {
-          if (isPartyPage(event.request.url)) {
-            return caches
-              .match(event.request)
-              .catch(() => caches.match('offline.html'));
-          } else {
-            return caches.match('offline.html');
-          }
-        }),
+        .catch(() => caches.match(event.request)),
     );
   }
 });
